4ee4aea1a0e067a60337c21daa23eaa2fb8c343f
rafael favaretto <rafael.favaretto3@gmail.com>
2023-09-11 20:53:29 -0300

    ap03(selecao): import e estrutura

---
+++ ExemploIf.java
@@ -0,0 +1,13 @@
+//java.lang é o pacote padrão do Java
+import javax.swing.JOptionPane;
+public  class ExemploIf{
+  static public void main(String [] args){
+    //declaração de variáveis
+    
+    //entrada
+
+    //processamento
+
+    //saída
+  }
+}
